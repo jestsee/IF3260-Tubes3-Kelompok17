efc339d7d1bd775ec0418d297228e990bfffbe68
rheael <rpandumpi@gmail.com>
2022-04-20 00:51:16 +0700

    add code to draw

--- js/draw.js
+++ js/draw.js
@@ -136,15 +136,8 @@ function _drawTexBump(arrPosition, matrix, withColor) {
                     normals[3*texSize*i+3*j+k] = 255*normalst[i][j][k];
 
         var textureBool = gl.getUniformLocation(program, "u_texture_bool");
+        var textureLocation = gl.getUniformLocation(program, "u_texture");
         var bumpBool = gl.getUniformLocation(program, "u_bump_bool");
-        var texCoordsArray = [];
-                        
-        var texCoord = [
-            vec2(0, 0),
-            vec2(0, 1),
-            vec2(1, 1),
-            vec2(1, 0)
-        ];
                         
         var modelViewMatrix, projectionMatrix, normalMatrix;
                         
@@ -168,20 +161,19 @@ function _drawTexBump(arrPosition, matrix, withColor) {
         //if ( !gl ) { alert( "WebGL isn't available" ); }
     
         gl.viewport( 0, 0, canvas.width, canvas.height );
-        gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+        //gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
         gl.enable(gl.DEPTH_TEST);
+
+        gl.useProgram( program );
 
         modelViewMatrix  = lookAt(eye, at, up);
         projectionMatrix = ortho(-0.75,0.75,-0.75,0.75,-5.5,5.5);
     
         var normalMatrix = mat4ToInverseMat3(modelViewMatrix); 
 
-        texCoordsArray.push(texCoord[0]);
-        texCoordsArray.push(texCoord[1]);
-        texCoordsArray.push(texCoord[2]);
-        texCoordsArray.push(texCoord[2]);
-        texCoordsArray.push(texCoord[3]);
-        texCoordsArray.push(texCoord[0]);
+        //var texcoordBuffer = gl.createBuffer();
+        //gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+        //setTexcoords(gl);
         
         var vBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
@@ -189,14 +181,15 @@ function _drawTexBump(arrPosition, matrix, withColor) {
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arrPosition), gl.STATIC_DRAW);
     
         var vPosition = gl.getAttribLocation(program, "vPosition");
-        gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+        gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(vPosition);
     
-        var tBuffer = gl.createBuffer();
-        gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer);
+        var texCoordBuffer = gl.createBuffer();
+        gl.bindBuffer( gl.ARRAY_BUFFER, texCoordBuffer);
         //gl.bufferData( gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoordsArray), gl.STATIC_DRAW);
-    
+        //gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
+        setTexcoords(gl);
+
         var vTexCoord = gl.getAttribLocation( program, "vTexCoord");
         gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(vTexCoord);
@@ -206,6 +199,7 @@ function _drawTexBump(arrPosition, matrix, withColor) {
         var texture = gl.createTexture();
         gl.activeTexture(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, texture);
+        // gl.uniform1i(gl.getUniformLocation( program, "Tex2"), 3);
         gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, texSize, texSize, 0, gl.RGB, gl.UNSIGNED_BYTE, normals);
         gl.generateMipmap(gl.TEXTURE_2D);
@@ -233,7 +227,8 @@ function _drawTexBump(arrPosition, matrix, withColor) {
 
         gl.uniform4fv( gl.getUniformLocation(program, "lightPosition"),flatten(lightPosition) );
         gl.uniform1i(textureBool, true);
-        //gl.uniform1i(bumpBool, true);
+        //gl.uniform1i(gl.getUniformLocation( program, "texMap"), 0);
+        gl.uniform1i(bumpBool, true);
 
         var primitiveType = gl.TRIANGLES;
         var offset = 0;
